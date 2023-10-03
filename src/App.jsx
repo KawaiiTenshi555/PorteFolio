c6bab28365d3e7a39222d667e75801f49f52cba1
Téo <teophile.chardon123@gmail.com>
2023-10-03 09:33:58 +0200

    Last change of 02/10

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
 
   return (
     <>
-      <Layout namepage={"Portefolio"}>
+      <Layout namepage={"Portfolio"}>
         <div className="bg-white">
           <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
             <div>
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,5 @@
 import { Typography } from "@material-tailwind/react";
- 
+
 export function SimpleFooter() {
   return (
     <footer className="flex w-full flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t border-red-700 py-6 text-center md:justify-between bottom-0 bg-gray-600 mt-auto">
@@ -20,11 +20,31 @@ export function SimpleFooter() {
         <li>
           <Typography
             as="a"
-            href="#"
+            href="/contact"
             color="blue-gray"
             className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500 text-white"
           >
             Contact Us
+          </Typography>
+        </li>
+        <li>
+          <Typography
+            as="a"
+            href="https://github.com/KawaiiTenshi555"
+            color="blue-gray"
+            className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500 text-white"
+          >
+            Github
+          </Typography>
+        </li>
+        <li>
+          <Typography
+            as="a"
+            href="https://www.linkedin.com/in/t%C3%A9ophile-chardon-90a69b27b/"
+            color="blue-gray"
+            className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500 text-white"
+          >
+            Linkedin
           </Typography>
         </li>
       </ul>
